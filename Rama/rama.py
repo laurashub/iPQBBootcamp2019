427a58e6d0aa54d3c2415cf09ecb22d4d28d1455
Laura Shub <shublaura@gmail.com>
2019-07-09 11:43:30 -0500

    Handles chains, saves plot to cwd

--- Rama/rama.py
+++ Rama/rama.py
@@ -3,6 +3,7 @@
 import sys, os
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def read_pdb(pdb_file):
 	if not os.path.isfile(pdb_file):
@@ -13,9 +14,13 @@ def read_pdb(pdb_file):
 	phi_groups = []
 	psi_groups = []
 	bb_atom_names = ["CA", "C", "N"]
+	chain = ""
 	with open(pdb_file) as f:
 		for line in f.read().splitlines():
 			if line[0:4] == "ATOM" and line[13:17].strip() in bb_atom_names:
+				if line[20:23].strip() != chain:
+					bb_atoms = []
+					chain = line[20:23].strip()
 				resn = int(line[23:27])
 				x = float(line[31:39])
 				y = float(line[39:47])
@@ -67,14 +72,25 @@ def calculate_angles(coordinates):
 		angles.append(calculate_angle(*coord))
 	return angles
 
-def main():
-	if len(sys.argv[1:]) < 1:
-		print("ERROR: No PDB file specified")
-		sys.exit(-1)
-	phis, psis = read_pdb(sys.argv[1])
-	phi_angles = calculate_angles(phis)
-	print(phi_angles)
+def graph_ramachandran(name, phi_angles, psi_angles):
+	fig, ax = plt.subplots()
+	ax.scatter(phi_angles, psi_angles, marker='.')
+	ax.set_xlim(-180, 180)
+	ax.set_ylim(-180, 180)
+	ax.set_adjustable('box')
+	ax.set_aspect(1./ax.get_data_ratio())
+	ax.set_xlabel("Φ")
+	ax.set_ylabel("Ψ")
+	fig.tight_layout()
+	fig.savefig(name + '_rama.png', dpi=500)
 
+if len(sys.argv[1:]) < 1:
+	print("ERROR: No PDB file specified")
+	sys.exit(-1)
 
-if __name__ == "__main__":
-	main()
+name = sys.argv[1].replace(".pdb", "")
+phis, psis = read_pdb(sys.argv[1])
+phi_angles = calculate_angles(phis)
+psi_angles = calculate_angles(psis)
+graph_ramachandran(name, phi_angles, psi_angles)
+
